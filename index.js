d4d1bcc50a05efdf6057165d8f7f9f3137121020
kaihengann <annkaiheng@gmail.com>
2019-06-25 11:45:15 +0800

    Add procfile

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-
+app.get('/greetings', (req, res, next) => {
+  console.log("Hello!");
+  res.status(200).json("Hello")
+})
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
